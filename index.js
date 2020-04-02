8086c58ca9e0a560c6cf2acddd9f96b2982c4461
Gavin Lazar Suntop <gavin@gsuntop.com>
2020-04-02 15:01:33 -0700

    use gzip compression

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
 require('dotenv').config()
 const { PORT } = process.env
 
+const compression = require('compression')
 const express = require('express')
 const app = express()
 
+app.use(compression())
 app.use(express.static('layouts'))
 
 app.get('/', (req, res) => res.send('<h1>Firefox Newtab Layout Service</h1><p>Status: <strong>NORMAL</strong></p>'))
